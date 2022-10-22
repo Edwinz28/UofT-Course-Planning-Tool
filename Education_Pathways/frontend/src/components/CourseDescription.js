220e8f0ab6ffa55faad9ef536723ba3ec4cb8fa9
Edwin.zhang28@gmail.com <edwin.zhang28@gmail.com>
2022-10-22 14:42:06 -0400

    Remove hard-coded course information. Reads from DB. Also removed some wonky front-end string parsing

--- Education_Pathways/frontend/src/components/CourseDescription.js
+++ Education_Pathways/frontend/src/components/CourseDescription.js
@@ -20,8 +20,8 @@ class CourseDescriptionPage extends Component {
     this.state = {
       course_code: "",
       course_name: "",
-      division: "Faculty of Applied Science and Engineering",
-      department: "Department of Edward S. Rogers Sr. Dept. of Electrical & Computer Engineering",
+      division: "",
+      department: "",
       graph : "",
       course_description: "",
       syllabus: "",
@@ -67,61 +67,24 @@ class CourseDescriptionPage extends Component {
       code: this.props.course_code
     })
       .then(res => {
-        console.log(res.data.course)
-        this.setState({course_code: res.data.course.code})
-        this.setState({course_name: res.data.course.name})
-        this.setState({certificate: res.data.course.certificate})
-        this.setState({course_description : res.data.course.description})
-        this.setState({graph: res.data.course.graph})
-        let prereq_len = res.data.course.prereq.length
-        if (prereq_len > 1) {
-          let prereq_str = ""
-          for (let i = 0; i < prereq_len; i++) {
-            prereq_str += res.data.course.prereq[i]
-            if (i !== prereq_len - 1) {
-              prereq_str += ", "
-            }
-          }
-          this.setState({prerequisites : prereq_str})
-        } else {
-          this.setState({prerequisites : res.data.course.prereq})
-        }
-        let coreq_len = res.data.course.coreq.length
-        if (coreq_len > 1) {
-          let coreq_str = ""
-          for (let i = 0; i < coreq_str; i++) {
-            coreq_str += res.data.course.coreq[i]
-            if (i !== coreq_len - 1) {
-              coreq_str += ", "
-            }
-          }
-          this.setState({corequisites : coreq_str})
-        } else {
-          this.setState({corequisites : res.data.course.coreq})
-        }
-        let exclusion_len = res.data.course.exclusion.length
-        if (exclusion_len > 1) {
-          let exclusion_str = ""
-          for (let i = 0; i < exclusion_str; i++) {
-            exclusion_str += res.data.course.exclusion[i]
-            if (i !== exclusion_len - 1) {
-              exclusion_str += ", "
-            }
-          }
-          this.setState({exclusions : exclusion_str})
-        } else {
-          this.setState({exclusions : res.data.course.exclusion})
-        }
-        let syllabus_link = "http://courses.skule.ca/course/" + this.state.course_code
-        this.setState({syllabus : syllabus_link})
-
+        this.setState({
+          course_code: res.data.course.code,
+          course_name: res.data.course.name,
+          certificate: res.data.course.certificate,
+          course_description : res.data.course.description,
+          graph: res.data.course.graph,
+          prerequisites : res.data.course.prereq,
+          exclusions : res.data.course.exclusion,
+          corequisites : res.data.course.coreq,
+          division: res.data.course.division,
+          department: res.data.course.department,
+          syllabus : "http://courses.skule.ca/course/" + res.data.course.code
+        })
+        
         let temp_graph = []
         //temp_graph.push(<ShowGraph graph_src={this.state.graph}></ShowGraph>)
         this.setState({graphics: temp_graph})
-
-
     })
-    console.log("new state: ", this.state)
   }
 
   openLink = () => {
