d7afb202815069a2cf8845fc3590aafe3c8c4401
yixin0289 <yixin0829@outlook.com>
2022-11-05 12:37:43 -0400

    Main changes forgot to push in the first commit

--- Education_Pathways/frontend/src/App.js
+++ Education_Pathways/frontend/src/App.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 import NavbarComp from "./components/Navbar.js";
 import FooterComp from "./components/Footer.js";
+import { Row, Col } from 'react-bootstrap';
 import './App.css';
 
 
 function App() {
 
   return (
-    <div>
-    <div className="App">
-      <NavbarComp  />
-    </div>
+    <div className='bg'
+      style={{ backgroundImage: `url("https://images.unsplash.com/photo-1576485976138-6c3cff620ae8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80")`
+    }}>
+      <div className="App">
+        <NavbarComp  />
+      </div>
 
-    <div className="App">
-      <FooterComp/>
-    </div>
+      {/* <div>
+        <FooterComp/>
+      </div> */}
     </div>
   );
 
