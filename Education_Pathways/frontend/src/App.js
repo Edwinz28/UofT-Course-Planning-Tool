de271ddee497d183fef607783894ca88d1c5fc7d
yixin0289 <yixin0829@outlook.com>
2022-11-06 11:54:17 -0500

    changed online URL for background image to local

--- Education_Pathways/frontend/src/App.js
+++ Education_Pathways/frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import NavbarComp from "./components/Navbar.js";
 import FooterComp from "./components/Footer.js";
-import { Row, Col } from 'react-bootstrap';
 import './App.css';
 
 
@@ -9,7 +8,7 @@ function App() {
 
   return (
     <div className='bg'
-      style={{ backgroundImage: `url("https://images.unsplash.com/photo-1576485976138-6c3cff620ae8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80")`
+      style={{ backgroundImage: `url("/ut-bg-min.jpg")`
     }}>
       <div className="App">
         <NavbarComp  />
