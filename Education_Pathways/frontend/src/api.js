d840e5ec14d5a0ff550e4c47210b2dc133cee750
RickyY1689 <richard.jr.yang@gmail.com>
2022-11-09 02:02:49 -0500

    updating base URL in frontend

--- Education_Pathways/frontend/src/api.js
+++ Education_Pathways/frontend/src/api.js
@@ -1,12 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-<<<<<<< HEAD
   baseURL: "https://education-pathways-plus.herokuapp.com/"
   //  baseURL: "http://localhost:5050/"
-=======
-
-  // baseURL: "https://lab3-docker.herokuapp.com/"
-  baseURL: "http://localhost:5050/"
->>>>>>> a2ac3524aedbf607c0e6d7cb5100a9f47ee6de43
 });
