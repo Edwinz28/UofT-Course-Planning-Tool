04ba7f8de2a58829ad2c91580cfd2853acd61408
Edwin.zhang28@gmail.com <edwin.zhang28@gmail.com>
2022-11-10 12:47:10 -0500

    interactive graph

--- Education_Pathways/frontend/src/components/CourseGraph.js
+++ Education_Pathways/frontend/src/components/CourseGraph.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import * as d3 from "d3"
 import { data, course_graph_data } from "./graph_data.js"
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch"
 
 // Copyright 2021 Observable, Inc.
 // Released under the ISC license.
@@ -37,8 +38,8 @@ function ForceGraph({
   nodeStrokeOpacity = 1, // node stroke opacity
   nodeRadius = 5, // node radius, in pixels
   nodeStrength,
-  linkSource = ({source}) => source, // given d in links, returns a node identifier string
-  linkTarget = ({target}) => target, // given d in links, returns a node identifier string
+  linkSource = ({ source }) => source, // given d in links, returns a node identifier string
+  linkTarget = ({ target }) => target, // given d in links, returns a node identifier string
   linkStroke = "#999", // link stroke color
   linkStrokeOpacity = 0.6, // link stroke opacity
   linkStrokeWidth = 1.5, // given d in links, returns a stroke width in pixels
@@ -60,8 +61,8 @@ function ForceGraph({
   const L = typeof linkStroke !== "function" ? null : d3.map(links, linkStroke);
 
   // Replace the input nodes and links with mutable objects for the simulation.
-  nodes = d3.map(nodes, (_, i) => ({id: N[i]}));
-  links = d3.map(links, (_, i) => ({source: LS[i], target: LT[i]}));
+  nodes = d3.map(nodes, (_, i) => ({ id: N[i] }));
+  links = d3.map(links, (_, i) => ({ source: LS[i], target: LT[i] }));
 
   // Compute default domains.
   if (G && nodeGroups === undefined) nodeGroups = d3.sort(G);
@@ -71,70 +72,70 @@ function ForceGraph({
 
   // Construct the forces.
   const forceNode = d3.forceManyBody(); // + = attraction - = repulsion
-  const forceLink = d3.forceLink(links).id(({index: i}) => N[i]); // .id() function let you use different field in node dict for linking
+  const forceLink = d3.forceLink(links).id(({ index: i }) => N[i]); // .id() function let you use different field in node dict for linking
   if (nodeStrength !== undefined) forceNode.strength(nodeStrength); // Define the repelling force
   if (linkStrength !== undefined) forceLink.strength(linkStrength); // Define the centre force
 
   const simulation = d3.forceSimulation(nodes)
-      .force("link", forceLink)
-      .force("charge", forceNode)
-      .force("center",  d3.forceCenter().strength(1))
-      .force("collide", d3.forceCollide(nodeRadius))
-      .on("tick", ticked);
+    .force("link", forceLink)
+    .force("charge", forceNode)
+    .force("center", d3.forceCenter().strength(1))
+    .force("collide", d3.forceCollide(nodeRadius))
+    .on("tick", ticked);
 
   const svg = d3.create("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("viewBox", [-width / 2, -height / 2, width, height])
-      .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [-width / 2, -height / 2, width, height])
+    .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
 
   const link = svg.append("g")
-      .attr("stroke", typeof linkStroke !== "function" ? linkStroke : null)
-      .attr("stroke-opacity", linkStrokeOpacity)
-      .attr("stroke-width", typeof linkStrokeWidth !== "function" ? linkStrokeWidth : null)
-      .attr("stroke-linecap", linkStrokeLinecap)
-      .selectAll("line")
-      .data(links)
-      .join("line");
+    .attr("stroke", typeof linkStroke !== "function" ? linkStroke : null)
+    .attr("stroke-opacity", linkStrokeOpacity)
+    .attr("stroke-width", typeof linkStrokeWidth !== "function" ? linkStrokeWidth : null)
+    .attr("stroke-linecap", linkStrokeLinecap)
+    .selectAll("line")
+    .data(links)
+    .join("line");
 
   const node = svg.append("g")
-      .attr("fill", nodeFill)
-      .attr("stroke", nodeStroke)
-      .attr("stroke-opacity", nodeStrokeOpacity)
-      .attr("stroke-width", nodeStrokeWidth)
-      .selectAll("circle")
-      .data(nodes)
-      .join("circle")
-      .attr("r", nodeRadius)
-      .call(drag(simulation))
-      .on("mouseenter", (evt, d) => { // callback function to highlight links and nodes on mouse over
-        node
-          .filter(n => n.id === d.id)
-          .attr("fill", "#f02b2b")
-          .attr("r", nodeRadius*2);
+    .attr("fill", nodeFill)
+    .attr("stroke", nodeStroke)
+    .attr("stroke-opacity", nodeStrokeOpacity)
+    .attr("stroke-width", nodeStrokeWidth)
+    .selectAll("circle")
+    .data(nodes)
+    .join("circle")
+    .attr("r", nodeRadius)
+    .call(drag(simulation))
+    .on("mouseenter", (evt, d) => { // callback function to highlight links and nodes on mouse over
+      node
+        .filter(n => n.id === d.id)
+        .attr("fill", "#f02b2b")
+        .attr("r", nodeRadius * 2);
 
-        link
-          .filter(l => l.source.id === d.id || l.target.id === d.id)
-          .attr("stroke", "#f02b2b")
-          .attr("stroke-opacity", 1.0)
-          .attr("stroke-width", 3);
-      })
-      .on("mouseleave", evt => { // callback function to reset links and nodes on mouse remove
-        node
-          .attr("r", nodeRadius);
+      link
+        .filter(l => l.source.id === d.id || l.target.id === d.id)
+        .attr("stroke", "#f02b2b")
+        .attr("stroke-opacity", 1.0)
+        .attr("stroke-width", 3);
+    })
+    .on("mouseleave", evt => { // callback function to reset links and nodes on mouse remove
+      node
+        .attr("r", nodeRadius);
 
-        if (G) node.attr("fill", ({index: i}) => color(G[i]));
+      if (G) node.attr("fill", ({ index: i }) => color(G[i]));
 
-        link.attr("display", "block")
-          .attr("stroke", typeof linkStroke !== "function" ? linkStroke : null)
-          .attr("stroke-opacity", linkStrokeOpacity)
-          .attr("stroke-width", typeof linkStrokeWidth !== "function" ? linkStrokeWidth : null);
-      });
+      link.attr("display", "block")
+        .attr("stroke", typeof linkStroke !== "function" ? linkStroke : null)
+        .attr("stroke-opacity", linkStrokeOpacity)
+        .attr("stroke-width", typeof linkStrokeWidth !== "function" ? linkStrokeWidth : null);
+    });
 
-  if (W) link.attr("stroke-width", ({index: i}) => W[i]);
-  if (L) link.attr("stroke", ({index: i}) => L[i]);
-  if (G) node.attr("fill", ({index: i}) => color(G[i]));
-  if (T) node.append("title").text(({index: i}) => T[i]);
+  if (W) link.attr("stroke-width", ({ index: i }) => W[i]);
+  if (L) link.attr("stroke", ({ index: i }) => L[i]);
+  if (G) node.attr("fill", ({ index: i }) => color(G[i]));
+  if (T) node.append("title").text(({ index: i }) => T[i]);
   if (invalidation != null) invalidation.then(() => simulation.stop());
 
   function intern(value) {
@@ -153,34 +154,34 @@ function ForceGraph({
       .attr("cy", d => d.y);
   }
 
-  function drag(simulation) {    
+  function drag(simulation) {
     function dragstarted(event) {
       if (!event.active) simulation.alphaTarget(1).restart();
       event.subject.fx = event.subject.x;
       event.subject.fy = event.subject.y;
     }
-    
+
     function dragged(event) {
       event.subject.fx = event.x;
       event.subject.fy = event.y;
     }
-    
+
     function dragended(event) {
       if (!event.active) simulation.alphaTarget(0);
       event.subject.fx = null;
       event.subject.fy = null;
     }
-    
+
     return d3.drag()
       .on("start", dragstarted)
       .on("drag", dragged)
       .on("end", dragended);
   }
 
-  return Object.assign(svg.node(), {scales: {color}});
+  return Object.assign(svg.node(), { scales: { color } });
 }
 
-function CourseGraph () {
+function CourseGraph() {
 
   // chart is a SVGSVGElement
   var chart = ForceGraph(course_graph_data, {
@@ -197,7 +198,7 @@ function CourseGraph () {
 
   const svg = useRef(null)
   useEffect(() => {
-    if(svg.current) {
+    if (svg.current) {
       svg.current.appendChild(chart)
     }
   }, [])
@@ -211,7 +212,11 @@ function CourseGraph () {
         </Row>
         <Row>
           <Col>
-            <div ref={svg}></div>
+            <TransformWrapper>
+              <TransformComponent>
+                <div ref={svg}></div>
+              </TransformComponent>
+            </TransformWrapper>
           </Col>
         </Row>
       </Container>
