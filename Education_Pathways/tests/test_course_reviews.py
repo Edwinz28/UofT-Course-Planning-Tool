60902ac155e63a5f41996f65b3c7b17acb96418f
yixin0829 <yixin0829@outlook.com>
2022-11-16 16:59:53 -0500

    added test cases for adding reviews to ECE444H1

--- Education_Pathways/tests/test_course_reviews.py
+++ Education_Pathways/tests/test_course_reviews.py
@@ -19,3 +19,21 @@ def test_get_review(course_code):
     tester = app.test_client()
     resp = tester.get(f"/course/reviews?course_code={course_code}")
     assert resp.status_code == 200
+
+# Written by Y.Tian
+@pytest.mark.parametrize("course_code, user_name, review, expected_code", [("ECE444", "BOT", "The best course.", 400),
+                                                                           ("ECE444H1", "BOT", "The best course ever!", 200),
+                                                                           ("ECE444H1", None, None, 400),
+                                                                           ("ECE444H1", "BOT", None, 400),
+                                                                           ("ECE444H1", None, "The project 1 is very in-depth.", 400)])
+def test_add_review_v2(course_code, user_name, review, expected_code):
+    tester = app.test_client()
+    if user_name is None and review is None:
+        resp = tester.post(f"/course/reviews?course_code={course_code}")
+    elif user_name is None:
+        resp = tester.post(f"/course/reviews?course_code={course_code}&review={review}")
+    elif review is None:
+        resp = tester.post(f"/course/reviews?course_code={course_code}&user_name={review}")
+    else:
+        resp = tester.post(f"/course/reviews?course_code={course_code}&user_name={user_name}&review={review}")
+    assert resp.status_code == expected_code
